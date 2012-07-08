8afbfae3972682a78a817be98799e67412c3fb29
esprit04 <adriendu04@hotmail.fr>
2012-07-08 21:11:10 +0200

    Correction du SQL

--- cms.sql
+++ cms.sql
@@ -4,28 +4,45 @@ Navicat MySQL Data Transfer
 Source Server         : Test
 Source Server Version : 50516
 Source Host           : localhost:3306
-Source Database       : onemu
+Source Database       : test
 
 Target Server Type    : MYSQL
 Target Server Version : 50516
 File Encoding         : 65001
 
-Date: 2012-07-08 20:56:06
+Date: 2012-07-08 21:10:41
 */
 
 SET FOREIGN_KEY_CHECKS=0;
 
 -- ----------------------------
--- Table structure for `banip`
+-- Table structure for `bannis`
 -- ----------------------------
-DROP TABLE IF EXISTS `banip`;
-CREATE TABLE `banip` (
-  `ip` varchar(15) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+DROP TABLE IF EXISTS `bannis`;
+CREATE TABLE `bannis` (
+  `pseudo` varchar(255) DEFAULT NULL,
+  `adresse_ip` char(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
--- Records of banip
+-- Records of bannis
 -- ----------------------------
+INSERT INTO `bannis` VALUES (null, '92.103.140.47');
+INSERT INTO `bannis` VALUES (null, '92.103.140.45');
+INSERT INTO `bannis` VALUES (null, '92.103.140.46');
+INSERT INTO `bannis` VALUES (null, '92.103.140.44');
+INSERT INTO `bannis` VALUES (null, '92.103.140.40');
+INSERT INTO `bannis` VALUES (null, '92.103.140.42');
+INSERT INTO `bannis` VALUES (null, '92.103.140.41');
+INSERT INTO `bannis` VALUES (null, '92.103.140.39');
+INSERT INTO `bannis` VALUES (null, '92.103.140.38');
+INSERT INTO `bannis` VALUES (null, '92.103.140.37');
+INSERT INTO `bannis` VALUES (null, '92.103.140.35');
+INSERT INTO `bannis` VALUES (null, '92.103.140.43');
+INSERT INTO `bannis` VALUES (null, '92.103.140.34');
+INSERT INTO `bannis` VALUES (null, '92.103.140.33');
+INSERT INTO `bannis` VALUES (null, '92.103.140.32');
+INSERT INTO `bannis` VALUES (null, '92.103.140.36');
 
 -- ----------------------------
 -- Table structure for `commentaires`
